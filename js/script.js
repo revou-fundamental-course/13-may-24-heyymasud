5226ade59b4f716a837742744356c2125cd9c59d
heyymasud <heyymasud@gmail.com>
2024-05-22 03:42:49 +0700

    feat: script done

--- js/script.js
+++ js/script.js
@@ -8,9 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const tinggi = Number(document.getElementById("tinggi-segitiga").value);
         const hasil = hitungLuasSegitiga(alas, tinggi);
         document.getElementById("hasil-segitigaLuas").innerHTML = 
-        `L = 1/2 x a x t
-        L = 1/2 x ${alas} x ${tinggi}
-        L = ${hasil}`;
+        `<p>L = 1/2 x a x t</p><p>L = 1/2 x ${alas} x ${tinggi}</p><p>L = ${hasil}</p>`;
     });
 
     formSegitigaKeliling.addEventListener("submit", function(event) {
@@ -19,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const sisiB = Number(document.getElementById("sisi-segitiga2").value);
         const sisiC = Number(document.getElementById("sisi-segitiga3").value);
         const hasil = hitungKelilingSegitiga(sisiA, sisiB, sisiC);
-        document.getElementById("hasil-segitigaKeliling").innerHTML = `Keliling Segitiga: ${hasil}`;
+        document.getElementById("hasil-segitigaKeliling").innerHTML = `<p>K = S1 + S2 + S3</p><p>K = ${sisiA} + ${sisiB} + ${sisiC}</p><p>L = ${hasil}</p>`;
     });
 });
 
